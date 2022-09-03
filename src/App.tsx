d8372ec43abedb22350a36cb894ba5af248fef66
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-09-03 20:08:18 -0300

    Created Repository

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import TodoTask from "./components/TodoTask/TodoTask";
+import { ITask } from "./Interfaces";
+import './styles/styles.css';
+
+function App() {
+	const [task, setTask] = useState("");
+	const [todoList, setTodoList] = useState<ITask[]>([])
+
+	function addTask(): void {
+		const idRandom = (num: number) => Math.floor(Math.random() * num)
+		
+		const newTask = { id: idRandom(999999999999999), nameTask: task }
+
+		setTodoList([...todoList, newTask])
+
+	}
+	
+	function deleteTask(DeleteTaskById: number): void {
+		setTodoList(todoList.filter((taskName) => taskName.id !== DeleteTaskById))
+
+	}
+
+	return (
+		<div className="App">
+
+			<header>
+
+				<h2>ToDo List React</h2>
+
+				<input
+					type="text" autoComplete="off" 
+					placeholder="Escrever task..." 
+					name="task"
+					className="input"
+					value={task}
+					onChange={(event) => setTask(event.target.value)}
+				/>
+
+				<button type="submit" onClick={addTask} className="btn-header">Adicionar Task</button>
+			</header>
+			
+			<div className="line"></div>
+
+			{todoList.map((task, key) => (
+				<TodoTask key={key} task={task} deleteTask={deleteTask} />
+			))}
+			
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/TodoTask/TodoTask.tsx
@@ -0,0 +1,24 @@
+import { ITask } from '../../Interfaces';
+import './styles.css';
+
+interface TaskProps {
+  task: ITask
+  deleteTask(DeleteTaskById: number): void,
+}
+
+function TodoTask({ task, deleteTask }: TaskProps) {
+	
+	return (
+		<div className="card">
+			<div>
+        <p>{task.nameTask}</p>
+      </div>
+
+      <div className="line2" >
+      <span className="btn-card" onClick={() => deleteTask(task.id)}>X</span>
+      </div>
+		</div>
+	);
+}
+
+export default TodoTask;
